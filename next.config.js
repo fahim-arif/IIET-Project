c58f59224b969c45bf67a7899009bd0f87e32fc1
fahim-two-match <fahim@twomatches.xyz>
2021-11-12 02:38:47 +0600

    next basic template

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    webpack: (config, {
+        buildId,
+        dev,
+        isServer,
+        defaultLoaders,
+        webpack
+    }) => {
+        // Note: we provide webpack above so you should not `require` it
+        // Perform customizations to webpack config
+        config.plugins.push(
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                "window.jQuery": "jquery"
+            })
+        );
+        // Important: return the modified config
+        return config;
+    }
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
+import Head from "next/head";
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
+import SimpleReactLightbox from 'simple-react-lightbox'
+// import css from 'file.css'
+
+//Initializations All Css
+import '../styles/index.css';
+// import '../styles/index.css.map';
+import '../styles/index.scss';
+
+
+
+// const newLocal = "../webpack.config";
+// const Slider = dynamic(() => import(newLocal), {
+//   ssr: false,
+// });
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <SimpleReactLightbox>
+
+      <Component {...pageProps} />
+
+    </SimpleReactLightbox>
+    <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" />
+    <Script src="/public/plugin/js/jquery-3.6.0.min.js" />
+    <Script src="/public/plugin/js/bootstrap.min.js" />
+  </>
 }
 
 export default MyApp
