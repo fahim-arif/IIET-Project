1232f73b2a04da0a2494c9aaf3876b2c6de4abcd
Shekhar kumar <shekharkumar235838@gmail.com>
2021-11-19 16:58:07 +0530

    added about-us page

--- components/pages/feedback-form/FeedForm.js
+++ components/pages/feedback-form/FeedForm.js
@@ -2,10 +2,10 @@ import React from "react";
 import styles from "../../../styles/services.module.css";
 import classes from "../../../styles/form.module.css";
 
-function EnquiryForm() {
+function FeedForm() {
   return (
-    <div className={styles.form}>
-      <h3 className={styles.formheading}>ENQUIRY ONLINE</h3>
+    <div className={styles.form} style={{marginLeft: window.innerWidth<800 ? 0 :'20px'}}>
+      <h4 style={{textAlign:"center",color:"tomato",fontWeight:"900"}}>We'd love to hear from you !</h4>
       <form className={styles.formContainer}>
         <div className={classes.inputgroup}>
           <input className={classes.input} required />
@@ -29,32 +29,6 @@ function EnquiryForm() {
         </div>
 
         <div className={classes.inputgroup}>
-          <select
-            className={classes.input}
-            required
-            style={{ borderRadius: 0 }}
-          >
-            <option value="Study Abroad">Study Abroad</option>
-            <option value="Immigration">Immigration</option>
-            <option value="Visa Assistance">Visa Assistance</option>
-            <option value="IELTS / GMAT / TOEFL">IELTS / GMAT / TOEFL</option>
-            <option value="Air Tickets">Air Tickets</option>
-            <option value="Travel Insurance">Travel Insurance</option>
-            <option value="Passport Online">Passport Online</option>
-            <option value="Training">Training</option>
-            <option value="Bank Loan Assistance">Bank Loan Assistance</option>
-            <option value="Forex Assistance">Forex Assistance</option>
-            <option value="Part Time Job Assistance">
-              Part Time Job Assistance
-            </option>
-            <option value="Workshop">Workshop</option>
-          </select>
-          <span className={classes.highlight}></span>
-          <span className={classes.bar}></span>
-          <label className={classes.label}>Services</label>
-        </div>
-
-        <div className={classes.inputgroup}>
           <textarea
             required
             className={classes.input}
@@ -62,7 +36,7 @@ function EnquiryForm() {
           />
           <span className={classes.highlight}></span>
           <span className={classes.bar}></span>
-          <label className={classes.label}>Comments</label>
+          <label className={classes.label}>Feedback</label>
         </div>
 
         <div className={classes.inputgroup}>
@@ -73,4 +47,4 @@ function EnquiryForm() {
   );
 }
 
-export default EnquiryForm;
+export default FeedForm;
